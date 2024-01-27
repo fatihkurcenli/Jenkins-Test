706ad990305a5fd3e8f1564b18391cd8408375ab
fkurcenli <fkurcenli@innova.com.tr>
2024-01-27 10:17:00 +0300

    Text değişikliği edildi.

--- app/src/main/java/com/autumnsun/jenkinsbase/MainActivity.kt
+++ app/src/main/java/com/autumnsun/jenkinsbase/MainActivity.kt
@@ -3,11 +3,14 @@ package com.autumnsun.jenkinsbase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = findViewById<TextView>(R.id.txt_hello_text)
+        a.text = "ChangedName"
         Log.e("second", "second")
     }
 }
